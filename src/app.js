7a5a2e2c61df0370a503e2b6fe3463ba0f39e974
Govind Thakur <govindthakur9540@gmail.com>
2021-04-06 01:42:35 +0530

    completed

--- src/app.js
+++ src/app.js
@@ -22,11 +22,20 @@ const PLAYERS = [
 ];
 
 // initialize players with image and strength
+
 const initPlayers = (players) => {
     let detailedPlayers = '';
 
     // Instead of forloop use Map method
     // Code here
+    detailedPlayers = players.map((player, i) =>{
+        return{
+            name: player,
+            image: "images/super-"+ (i+1) + ".png",
+            strength: getRandomStrength(),
+            type: (i%2 == 0)? "hero":"villain"
+        }
+    });
 
     return detailedPlayers;
 }
@@ -43,7 +52,12 @@ const buildPlayers = (players, type) => {
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
-
+    fragment = players.filter(player => player.type == type).map(player=>
+        `<div class="player">
+            <img src="${player.image}">
+            <div class="name">${player.name}</div>
+            <div class="strength">${player.strength}</div>
+        </div>`).join()
     return fragment;
 }
 
